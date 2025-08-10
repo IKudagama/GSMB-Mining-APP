581d674d2915f3e35da334a39ead4ae957d50f08
Tharushi Hansima <tharushihansima23@gmail.com>
2025-08-10 16:10:41 +0530

    add tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { AppProvider } from './context/AppContext.jsx'
 import { LangProvider } from './context/LangContext.jsx'
 import ToastProvider from './components/ToastProvider.jsx'
 import './styles.css'
+import './index.css';
+
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
